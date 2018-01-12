a08ecbaeded7f4f67c7c31d571f9ee146bd7b908
Bougou <bougou@126.com>
2018-01-12 18:33:56 +0800

    add script

--- quickboot-vm-centos6.sh
+++ quickboot-vm-centos6.sh
@@ -11,13 +11,13 @@ vmtmpl=$1
 vmname=$2
 vmip=$3
 
-python ./deploy-vm-centos7.py \
+python ./deploy-vm-centos6.py \
   --conf ./deploy-vm.conf \
   --tmpl "$vmtmpl" \
   --name "$vmname" \
-  --cpu 2 \
-  --mem 4 \
-  --sys 40 \
+  --cpu 1 \
+  --mem 2 \
+  --sys 20 \
   --net virbr1/${vmip}/24 \
   --gw 172.18.28.1
 
